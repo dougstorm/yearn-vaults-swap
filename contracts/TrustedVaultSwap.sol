288db41e82f2ed351166798e41fd6929d9eb1272
dougstorm <dougstorm@users.noreply.github.com>
2021-02-09 15:25:28 -0600

    feat:add governance setters and minor optimizations

--- contracts/TrustedVaultSwap.sol
+++ contracts/TrustedVaultSwap.sol
@@ -7,20 +7,37 @@ import "./VaultSwap.sol";
 import "../interfaces/IRegistry.sol";
 
 contract TrustedVaultSwap is VaultSwap {
-
-    address public immutable registry;
+    address public governance;
+    address public pendingGovernance;
+    address public registry;
 
     modifier onlyRegisteredVault(address vault) {
         require(
-            IRegistry(registry)
-                .latestVault(IVaultAPI(vault).token()) == vault
-            , "Target vault should be the latest for token");
+            IRegistry(registry).latestVault(IVaultAPI(vault).token()) == vault,
+            "Target vault should be the latest for token"
+        );
         _;
-    } 
+    }
 
-    constructor(address _registry) VaultSwap() public {
+    modifier onlyGovernance {
+        require(
+            msg.sender == governance,
+            "Only governance can call this function."
+        );
+        _;
+    }
+    modifier onlyPendingGovernance {
+        require(
+            msg.sender == pendingGovernance,
+            "Only pendingGovernance can call this function."
+        );
+        _;
+    }
+
+    constructor(address _registry) public VaultSwap() {
         require(_registry != address(0), "Registry cannot be 0");
 
+        governance = address(0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52);
         registry = _registry;
     }
 
@@ -28,7 +45,31 @@ contract TrustedVaultSwap is VaultSwap {
         address vaultFrom,
         address vaultTo,
         uint256 shares
-    ) onlyRegisteredVault(vaultTo) override internal {
+    ) internal override onlyRegisteredVault(vaultTo) {
         super._swap(vaultFrom, vaultTo, shares);
+    }
+
+    function sweep(address _token) external onlyGovernance {
+        IERC20(_token).safeTransfer(
+            governance,
+            IERC20(_token).balanceOf(address(this))
+        );
+    }
+
+    function acceptGovernance() external onlyPendingGovernance {
+        governance = msg.sender;
+    }
+
+    // setters
+    function setPendingGovernance(address _pendingGovernance)
+        external
+        onlyGovernance
+    {
+        pendingGovernance = _pendingGovernance;
+    }
+
+    function setRegistry(address _registry) external onlyGovernance {
+        require(_registry != address(0), "Registry cannot be 0");
+        registry = _registry;
     }
 }
